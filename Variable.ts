2e283e8968e4def854a00bb08e4cbd2fc50744e8
hq <602997434@qq.com>
2022-10-26 22:38:49 +0800

    变量

--- Variable.ts
+++ Variable.ts
@@ -1,2 +1,22 @@
-// 变量声明
-let nm = "Hello World!"
+// 字符串变量声明
+let nm:string = "Hello World!";
+
+// 数字常量声明
+const tmp:number = 111.1;
+
+// 布尔变量声明
+let exist:boolea = true;
+
+// 任何类型声明
+let ex:any = false;
+ex = 1;
+
+// 数组类型声明
+let a:number[] = [1, 2, 3, 4, 5, 6];
+
+// 变量类型
+number：数字类型
+string：字符串类型
+boolean：布尔类型
+any：任何类型
+array：数组
