17c584623f18be4a6d2fa9f49e2379b1af86f082
hq <602997434@qq.com>
2022-10-27 22:14:40 +0800

    类

---
+++ Class.ts
@@ -0,0 +1,19 @@
+class Person{
+        static des: string = "这是一个类";
+        name: string = "";
+        age: number = "";
+
+        say() {
+              document.write("haha");
+        }
+};
+
+// 抽象类
+class Student extends Person{
+
+};
+
+abstract class Person{
+        name: string = "";
+        abstract say();
+}
