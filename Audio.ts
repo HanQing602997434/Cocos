56d5ff175ec403f6ac2565244e0e7765b591b414
hq <602997434@qq.com>
2022-11-26 23:13:50 +0800

    音频

--- Audio.ts
+++ Audio.ts
@@ -9,4 +9,35 @@ cc.loader.loadRes("haha", cc.AudioClip, (res, clip)=>{
         player.clip = clip;
         // 播放音频
         player.play();
+        // 是否正在播放
+        player.isPlaying
+        // 暂停
+        player.pause();
+        // 恢复
+        player.resume();
+        // 停止
+        player.stop();
+        // 是否循环播放
+        player.loop = true;
+        // 音量大小
+        player.volume = 1;
 });
+
+cc.loader.loadRes("haha", cc.AudioClip, (res, clip)=>{
+        // 播放
+        let audioId: number = cc.audioEngine.playMusic(clip, true);
+        // 是否正在播放
+        cc.audioEngine.isMusicPlaying();
+        // 暂停
+        cc.audioEngine.pause(audioId);
+        // 暂停背景音乐，不需要id
+        cc.audioEngine.pauseMusic();
+        // 恢复
+        cc.audioEngine.resume(audioId);
+        // 停止
+        cc.audioEngine.stop(audioId);
+        // 循环
+        cc.audioEngine.setLoop(audioId, true);
+        // 声音大小
+        cc.audioEngine.setVolume(audioId, 1);
+})
