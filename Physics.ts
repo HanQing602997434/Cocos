4e31ba4a0e1dd1f002d0e2ef64152d18204f746a
hq <602997434@qq.com>
2022-11-27 21:46:53 +0800

    物理系统

--- Physics.ts
+++ Physics.ts
@@ -5,3 +5,10 @@
 比如刚体，盒体碰撞
 
 物体想要有物理，先加上刚体组件
+
+// 获取刚体
+let rbody = this.getComponent(cc.RigidBody);
+
+// 力
+rbody.applyForce(cc.v2(1000, 0), cc.v2(0, 0), true);
+rbody.applyForceToCenter(cc.v2(5000, 0), true);
