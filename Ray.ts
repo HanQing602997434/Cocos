8f25011396097f259c739fd8ea214c03db6135e6
hq <602997434@qq.com>
2022-11-29 21:15:12 +0800

    射线

--- Ray.ts
+++ Ray.ts
@@ -1,1 +1,11 @@
 // 射线
+let results = cc.director.getPhysicsManager().rayCast(this.node.getPosition(), cc.v2(this.node.x, this.node.y + 100), cc.Ray)
+
+// 射线碰到的碰撞器
+res.collider
+
+// 碰到的点
+res.point
+
+// 碰到的法线
+res.normal
